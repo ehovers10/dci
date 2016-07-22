a237bef5bd16a6338b488d5a7225af785f42c49f
ehovers10 <ehovers10@gmail.com>
2016-07-22 15:53:15 -0700

    good progress on relations

--- js/bunch.js
+++ js/bunch.js
@@ -1,4 +1,22 @@
 $(document).ready( function() {
+
+  // Set margin of hidden areas
+  $(".area").each( function() {
+      var bunchwidth = $(this).parent().width() / 2,
+          areawidth = $(this).width() / 2,
+          offset = bunchwidth - areawidth;
+      $( this ).css( "margin-left", offset );
+  });
+
+  // Set margin of shown area
+  $(".area:first-of-type").each( function() {
+    var bunchwidth = $(this).parent().width() / 2,
+        areawidth = $(this).children().width() / 2,
+        offset = bunchwidth - areawidth;
+    $( this ).css( "margin-left", offset );
+  });
+
+  // Toggle areas
   $( ".body-nav a" ).click( function() {
     var temp = $( this ).attr( "class" );
     $( this ).closest( ".bunch" ).find( ".area" ).each( function() {
